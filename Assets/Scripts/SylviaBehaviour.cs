1fa543dfd0490002d949989d13ca275c7a2ac4eb
DrewForYou <andrewdrees214@yahoo.com>
2022-10-04 00:33:36 -0500

    Gravity Editing Code

--- Assets/Scripts/SylviaBehaviour.cs
+++ Assets/Scripts/SylviaBehaviour.cs
@@ -6,9 +6,18 @@ public class SylviaBehaviour : MonoBehaviour
 {
     private bool isDiving;
     private bool isShadowMorphed;
-    
+    private bool isGliding;
+
+    //Different fall speed limits for Syvlia
+    public float GlideSpeedLimit;
     public float FallSpeedLimit;
     public float DiveSpeedLimit;
+
+    //Gravity Scales for Normal, Glide, Dive. I feel like this was what I was mean to do for the fall speed limits
+    public float GlideGravity;
+    public float NormalGravity;
+    public float DiveGravity;
+
     public float SylviaSpeed;
     public bool IsInAir;
     public int AirJumpsLeft;
@@ -49,11 +58,80 @@ public class SylviaBehaviour : MonoBehaviour
             }
         }
 
-        if(isDiving == false)
+        //Gliding and Diving are meant to be held down in order to allow for their code to cancel eachother out
+        //Having it be press only may be able to work but I'm choosing to go this route
+
+        //Glide Code
+
+        //When Glide Is Pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isGliding = true;
+            //Gliding and Diving Cancel eachother out
+            isDiving = false;
+
+            //Setting gravity
+            GetComponent<Rigidbody2D>().gravityScale = GlideGravity;
+            Debug.Log("Glide");
+        }
+
+        //When Glide Is Released
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isGliding = false;
+            //Reseting Gravity
+            GetComponent<Rigidbody2D>().gravityScale = NormalGravity;
+        }
+
+
+        //Diving Code
+
+        //When Diving Is Pressed
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isDiving = true;
+            //Gliding and Diving Cancel eachother out
+            isGliding = false;
+
+            //Setting Gravity
+            GetComponent<Rigidbody2D>().gravityScale = DiveGravity;
+            Debug.Log("Dive");
+        }
+
+        //When Diving Is Released
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            isDiving = false;
+            //Reseting Gravity
+            GetComponent<Rigidbody2D>().gravityScale = NormalGravity;
+        }
+
+
+        //Fall Speed Limit Code
+        
+        //Glide Speed
+        if (isGliding)
         {
             hold = GetComponent<Rigidbody2D>().velocity;
+            //the Mathf.clamp limites the minimum fall speed to the GlideSpeedLimit, and sets the maximum to the sum of
+            //AirJumpForce and GroundJumpForce to make sure it will always be high enough to not interfere with jumping velocity
+            GetComponent<Rigidbody2D>().velocity = new Vector2(hold.x, Mathf.Clamp(hold.y, GlideSpeedLimit, AirJumpForce + GroundJumpForce));
+        }
 
-            //the Mathf.clamp limites the minimum fall speed to the fall speed limit, and sets the maximum to the sum of
+        //Dive Speed
+        else if(isDiving)
+        {
+            hold = GetComponent<Rigidbody2D>().velocity;
+            //the Mathf.clamp limites the minimum fall speed to the DiveSpeedLimit, and sets the maximum to the sum of
+            //AirJumpForce and GroundJumpForce to make sure it will always be high enough to not interfere with jumping velocity
+            GetComponent<Rigidbody2D>().velocity = new Vector2(hold.x, Mathf.Clamp(hold.y, DiveSpeedLimit, AirJumpForce + GroundJumpForce));
+        }
+
+        //Normal Fall Speed
+        else
+        {
+            hold = GetComponent<Rigidbody2D>().velocity;
+            //the Mathf.clamp limites the minimum fall speed to the FallSpeedLimit, and sets the maximum to the sum of
             //AirJumpForce and GroundJumpForce to make sure it will always be high enough to not interfere with jumping velocity
             GetComponent<Rigidbody2D>().velocity = new Vector2(hold.x, Mathf.Clamp(hold.y, FallSpeedLimit, AirJumpForce + GroundJumpForce));
         }
