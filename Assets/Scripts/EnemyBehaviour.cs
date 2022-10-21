ac97374accf8fc3acabb42c200e399e6ed93ea5a
DrewForYou <andrewdrees214@yahoo.com>
2022-10-21 15:45:03 -0500

    Enemy moves and ShadowGlass

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -6,14 +6,23 @@ public class EnemyBehaviour : MonoBehaviour
 {
     //public bool CheckForShadowmorph;
     public SylviaBehaviour Sylvia;
+    public Transform LeftBoundry;
+    public Transform RightBoundry;
+    //public Vector2 LeftLocate;
+    //public Vector2 RightLocate;
+    public float EnemySpeed;
+    public float EnemySize;
 
     private void Start()
     {
         Sylvia = FindObjectOfType<SylviaBehaviour>();
+        //LeftLocate = new Vector2(LeftBoundry.position.x, LeftBoundry.position.y);
+        //RightLocate = new Vector2(RightBoundry.position.x, RightBoundry.position.y);
     }
 
     private void Update()
     {
+        //Checks if Sylvia is shadowmorphed and turns off collision when Sylvia is Shadowmoprhed true and on when she is not
         if (Sylvia.IsShadowMorphed)
         {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>());
@@ -22,6 +31,18 @@ public class EnemyBehaviour : MonoBehaviour
         {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>(), false);
         }
+        //Movement code
+        Vector2 movePos = gameObject.transform.position;
+        movePos.x -= EnemySpeed * Time.deltaTime;
+        //Setting Boundries
+        movePos.x = Mathf.Clamp(movePos.x, LeftBoundry.position.x, RightBoundry.position.x);
+
+        //Switches direction when they reach the boundry
+        if(movePos.x == LeftBoundry.position.x || movePos.x == RightBoundry.position.x)
+        {
+            EnemySpeed *= -1;
+        }
+        gameObject.transform.position = movePos;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -36,6 +57,7 @@ public class EnemyBehaviour : MonoBehaviour
                 Sylvia.Hurt();
         }
         */
+        //hurts Sylvia when she is collided with
         if (collision.gameObject.GetComponent<SylviaBehaviour>())
         {
             Sylvia.Hurt();
--- Assets/Scripts/ShadowGlassBehaviour.cs
+++ Assets/Scripts/ShadowGlassBehaviour.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ShadowGlassBehaviour : MonoBehaviour
 {
-    /*public SylviaBehaviour Sylvia;
+    public SylviaBehaviour Sylvia;
+    public bool DiveThrough;
 
     private void Start()
     {
@@ -13,7 +14,13 @@ public class ShadowGlassBehaviour : MonoBehaviour
 
     private void Update()
     {
+        //Checks if Sylvia is shadowmorphed and turns off collision when Sylvia is Shadowmoprhed true and on when she is not
+        //note you can dive thorugh the walls even if you are standing on them.
         if (Sylvia.IsShadowMorphed)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>());
+        }
+        else if(DiveThrough && Sylvia.IsDiving)
         {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>());
         }
@@ -21,5 +28,11 @@ public class ShadowGlassBehaviour : MonoBehaviour
         {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>(), false);
         }
-    }*/
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Use to eject player from object if they get stuck in it.
+    }
 }
