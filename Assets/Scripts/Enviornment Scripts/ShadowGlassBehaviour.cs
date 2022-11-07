18c0bdd62ce7f1248143d5f823acd31aa7512bd6
DrewForYou <andrewdrees214@yahoo.com>
2022-11-07 13:08:42 -0600

    After Playtest 1 before edits

--- Assets/Scripts/Enviornment Scripts/ShadowGlassBehaviour.cs
+++ Assets/Scripts/Enviornment Scripts/ShadowGlassBehaviour.cs
@@ -16,13 +16,13 @@ public class ShadowGlassBehaviour : MonoBehaviour
     {
         //Checks if Sylvia is shadowmorphed and turns off collision when Sylvia is Shadowmoprhed true and on when she is not
         //note you can dive thorugh the walls even if you are standing on them.
-        if (Sylvia.IsShadowMorphed)
-        {
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>());
-        }
-        else if(DiveThrough && Sylvia.IsDiving)
+        if (DiveThrough && Sylvia.IsDiving)
         {
             Debug.Log("Dive");
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>());
+        }
+        else if (Sylvia.IsShadowMorphed)
+        {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Sylvia.GetComponent<Collider2D>());
         }
         else
